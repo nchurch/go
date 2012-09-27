79e559a8512562508722009695bd3257229b6ad1
Nicholas Chubrich <nchubrich@gmail.com>
2012-09-26 17:11:37 -0700

    solves go now

--- src/go/core.clj
+++ src/go/core.clj
@@ -2,36 +2,7 @@
   (:refer-clojure :exclude [==])
   (:use clojure.core.logic))
 
-(defn dead-equals [self vert hor]
-  (== (vert 0) (vert 1))
-  (== (hor 0) (hor 1))
-  (== (hor 0) (vert 0))
-  (== (vert 0) self))
-
-(defn dead-half-equals [self vert hor]
-  (== (vert 0) (vert 1))
-  (== (hor 0) (hor 1))
-  (== (vert 0) self))
-
-(defn dead-vert-unequal [self vert hor]
-  (!= (vert 0) (vert 1))
-  (conde [
-          (== (vert 0) self)
-          (== (vert 1) self)]))
-
-(defn dead-hor-unequal [self vert hor]
-  (!= (hor 0) (hor 1))
-  (conde [
-          (== (hor 0) self)
-          (== (hor 1) self)]))
-
-(defn dead2 [self vert hor]
-  (conde
-    [
-    (dead-equals self vert hor)
-    (dead-vert-unequal self vert hor)
-    (dead-hor-unequal self vert hor)]))
-
+;example board
 (def board
   [[:e :e :e :e :e :e :e]
    [:e :s :s :s :s :s :e]
@@ -59,38 +30,47 @@
 (defn bottom [[x y]]
   [x, (inc y)])
 
-(defn color [self]
-  ((board (self 1)) (self 0)))
+(defn color [piece board]
+  ((board (piece 1)) (piece 0)))
 
-(defn alive [type self]
-  (conde 
-    ((== (color self) :e) u#)
-    ((!= (color self) type) u#)
-    ((== (color self) :s) s#)
-    (s# (conde 
-      ((alive type (right self)))
-      ((alive type (left self)))
-      ((alive type (top self)))
-      ((alive type (bottom self)))))))
+(defn alive [piece board]
+  (let [visited (atom #{})]
+    (letfn [(open [pcolor piece board]
+                  (cond
+                    (some #{piece} @visited) u#
+                    (= (color piece board) :s) s#
+                    (= (color piece board) :e) u#
+                    (not= (color piece board) pcolor) u#
+                    :else (r-alive pcolor piece board)))
+            (r-alive [pcolor piece board]
+              (do
+                (swap! visited conj piece)
+                (conde
+                  [(open pcolor (right piece) board)]
+                  [(open pcolor (left piece) board)]
+                  [(open pcolor (top piece) board)]
+                  [(open pcolor (bottom piece) board)])))]
+      (r-alive (color piece board) piece board))))
+                
+(defn keep-all [piece board]
+  (run* [q]
+        (alive piece board)
+        (== q true)))
 
-(defn blocking? [blockee blocker]
-  (conde
-    ((== (color blockee) :e) u#)
-    ((== (color blocker) :e))
-    ((!= (color blocker) (color blockee)))
-    (:else (dead blocker))))
-  
-(defn dead [self]
-  (all 
-    (blocking? self (right self))
-    (blocking? self (left self))
-    (blocking? self (top self))
-    (blocking? self (bottom self))))
+(defn keep? [piece board]
+  (first (run 1 [q]
+        (alive piece board)
+        (== q true))))
 
-(defn keep? [typee item]      
-  (run 1 [q]
-        (alive typee item)
-        (== true q)))
+(defn test-all-pieces [board]
+  (for [y (range 0 (count board))]
+    (for [x (range 0 (count (board 0)))]
+      (cond (= (color [x y] board) :e) ":e  "
+            (= (color [x y] board) :s) ":s  "
+            :else (if (keep? [x y] board)
+                    (str (color [x y] board) " " "t")
+                    (str (color [x y] board) " " "f"))))))
+    
        
 
 
